b72400270d35c24b22c14108bea231af4d19f33d
Epikle <98042337+Epikle@users.noreply.github.com>
2022-10-18 19:04:08 +0300

    Added intersection observer

--- src/dtc/components/DtcList.js
+++ src/dtc/components/DtcList.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
 import DtcError from './DtcError';
 import DtcLoading from './DtcLoading';
@@ -10,24 +12,36 @@ import './DtcList.css';
 
 const DtcList = ({ search }) => {
   const { isLoading, isError, data, error } = useQuery(['dtcList'], getDtcList);
+  const [listItems, setListItems] = useState(100);
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    rootMargin: '0px 0px 50px 0px',
+  });
+
+  useEffect(() => {
+    if (search) {
+      setListItems(100);
+      window.scrollTo(0, 0);
+    }
+    if (inView) {
+      setListItems((prevS) => prevS + 100);
+    }
+  }, [inView, search]);
 
   if (isError) return <DtcError error={error.message} />;
   if (isLoading) return <DtcLoading placeholders={36} />;
 
   const dtcsToShow = !search
-    ? data.slice(0, 100)
+    ? data.slice(0, listItems)
     : data
         .filter((dtc) =>
           search
             .trim()
             .split(' ')
-            .some(
-              (w) =>
-                dtc.code.title.toLowerCase().includes(w.toLowerCase()) ||
-                dtc.code.description.toLowerCase().includes(w.toLowerCase())
-            )
+            .some((w) => dtc.code.title.toLowerCase().includes(w.toLowerCase()))
         )
-        .slice(0, 10);
+        .slice(0, 50);
 
   if (dtcsToShow.length === 0) return <DtcNotFound />;
 
@@ -36,6 +50,7 @@ const DtcList = ({ search }) => {
       {dtcsToShow.map((dtc) => (
         <DtcItem key={dtc.id} dtc={dtc} />
       ))}
+      {!search && <li ref={ref}></li>}
     </ul>
   );
 };
