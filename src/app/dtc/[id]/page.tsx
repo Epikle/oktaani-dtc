a6ff20f01c231e5a0e416cdfc2129d7fa1a847ea
Epikle <98042337+Epikle@users.noreply.github.com>
2023-10-04 15:03:44 +0300

    :sparkles: Add metadata

--- src/app/dtc/[id]/page.tsx
+++ src/app/dtc/[id]/page.tsx
@@ -3,6 +3,15 @@ import { getDtcData } from './actions';
 
 import styles from './page.module.css';
 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const dtcData = await getDtcData(params.id);
+
+  return {
+    title: `${params.id} | oktaaniDTC`,
+    description: dtcData?.code.description,
+  };
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const dtcData = await getDtcData(params.id);
 
