692c90ea905367f520a72eaf76d941e8406ab1ab
Epikle <98042337+Epikle@users.noreply.github.com>
2023-10-10 14:23:01 +0300

    :art: Style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
-  return <footer className={styles.footer}>2023 &copy; oktaani.com</footer>;
+  return (
+    <footer className={styles.footer}>
+      2023 &copy;{' '}
+      <a href="https://oktaani.com/" rel="noopener noreferrer" target="_blank">
+        oktaani.com
+      </a>
+    </footer>
+  );
 }
--- src/components/dtc/Code.tsx
+++ src/components/dtc/Code.tsx
@@ -1,10 +1,11 @@
 import { CSSProperties } from 'react';
+import { notFound } from 'next/navigation';
 import { Dtc, Systems } from '@prisma/client';
+import { BarChart3, Book, BrainCog, Car, LocateFixed } from 'lucide-react';
+
+import { Explanation } from './Explanation';
 
 import styles from './Code.module.css';
-import { Explanation } from './Explanation';
-import { notFound } from 'next/navigation';
-import { Book, BrainCog, Car, LocateFixed } from 'lucide-react';
 
 export default async function Code({ dtc, className }: { dtc: Dtc | null; className?: string }) {
   if (!dtc) return notFound();
@@ -18,9 +19,14 @@ export default async function Code({ dtc, className }: { dtc: Dtc | null; classN
 
   return (
     <div className={className}>
-      <h2 className={styles.header} style={codeStyle[dtc.systemTitle]}>
-        {dtc.codeTitle} <span>{dtc.systemTitle}</span>
-      </h2>
+      <div className={styles['header-container']}>
+        <h2 className={styles.header} style={codeStyle[dtc.systemTitle]}>
+          {dtc.codeTitle} <span>{dtc.systemTitle}</span>
+        </h2>
+        <div className={styles.views}>
+          <BarChart3 /> <span>{dtc.views}</span> views
+        </div>
+      </div>
       <ul className={styles.list}>
         <li>
           <span>
