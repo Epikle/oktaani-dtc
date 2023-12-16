0562758c59cfce3cb204a93d12a594d95b94a832
Epikle <98042337+Epikle@users.noreply.github.com>
2023-12-16 19:07:08 +0200

    :recycle: Update db schema

--- prisma/migrations/20231005155032_test/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[codeTitle]` on the table `Dtc` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Dtc_codeTitle_key" ON "Dtc"("codeTitle");
--- prisma/migrations/20231216170519_init/migration.sql
+++ prisma/migrations/20231216170519_init/migration.sql
@@ -1,17 +1,23 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "dtc";
+
 -- CreateEnum
-CREATE TYPE "Systems" AS ENUM ('Powertrain', 'Network', 'Body', 'Chassis');
+CREATE TYPE "dtc"."Systems" AS ENUM ('Powertrain', 'Network', 'Body', 'Chassis');
 
 -- CreateTable
-CREATE TABLE "Dtc" (
+CREATE TABLE "dtc"."Dtc" (
     "id" TEXT NOT NULL,
     "codeTitle" TEXT NOT NULL,
     "codeDescription" TEXT NOT NULL,
     "codeLocation" TEXT,
     "systemCode" TEXT NOT NULL,
-    "systemTitle" "Systems" NOT NULL,
+    "systemTitle" "dtc"."Systems" NOT NULL,
     "systemName" TEXT NOT NULL,
     "views" INTEGER NOT NULL DEFAULT 0,
     "gptInfo" TEXT,
 
     CONSTRAINT "Dtc_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Dtc_codeTitle_key" ON "dtc"."Dtc"("codeTitle");
